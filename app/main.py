59f162b9d1002f7642994b9216bb98331ce2088a
YoohaBae <yooha.bae.2@gmail.com>
2022-09-11 20:14:08 -0400

    initial code

---
+++ app/main.py
@@ -0,0 +1,34 @@
+"""
+GuROOM backend main.py
+"""
+
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+app.include_router(apps_router, prefix="/apps")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://guroom.live"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def test():
+    return JSONResponse({'message': 'test'})
+
+
+def run():
+    uvicorn.run(app)
+
+if __name__ == "__main__":
+    run()
+
---
+++ app/micro_apps/test/__init__.py
@@ -0,0 +1,9 @@
+"""
+    prefix: /apps/test
+"""
+
+from fastapi import APIRouter
+from .endpoints import router as endpoints_router
+
+router = APIRouter()
+router.include_router(endpoints_router)

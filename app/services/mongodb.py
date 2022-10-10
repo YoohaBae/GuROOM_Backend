de6b8c929c23897a2abab1c8084483384fe0ca5c
jinno92 <78394681+jinno92@users.noreply.github.com>
2022-10-10 17:23:01 -0400

    Update mongodb.py

--- app/services/mongodb.py
+++ app/services/mongodb.py
@@ -1,36 +1,4 @@
 import pymongo
-<<<<<<< HEAD
-import os
-
-
-class MongoDB:
-    def __init__(self):
-        URL = os.getenv("URL")
-        client = pymongo.MongoClient(URL)
-        db_name = os.getenv("DB_NAME")
-        self.db = client[db_name]
-
-    def get_all_collections(self, collection_name):
-        return self.db.collection_name.find().pretty()
-
-    def insert_document(self, collection_name, data):
-        self.db.collection_name.insert(data)
-
-    def delete_collection(self, collection_name, value):
-        self.db.collection_name.delectOne(value)
-
-    def delete_multiple_collections(self, collection_name, value):
-        self.db.collection_name.delectMany(value)
-
-    def update_document(self, collection_name, original, desired):
-        self.db.collection_name.update(original, desired)
-
-    def find_document(self, collection_name, value):
-        return self.db.collection_name.findOne(value).pretty()
-
-    def find_multiple_documents(self, collection_name, value):
-        return self.db.collection_name.find(value).pretty()
-=======
 
 
 class MongoDB:
@@ -70,4 +38,3 @@ class MongoDB:
 
     def update_documents(self, collection_name: str, query: dict, new_data: dict):
         self.db[collection_name].update_one(query, new_data)
->>>>>>> c4552de81d54319556ca3b4a937a9b0cd7002e83

c7a59b40da78d8e056fb7adbe97b712b6e743788
jinno92 <78394681+jinno92@users.noreply.github.com>
2022-10-10 16:58:00 -0400

    fixed interface class

--- app/services/mongodb.py
+++ app/services/mongodb.py
@@ -4,42 +4,28 @@ import os
 
 class MongoDB:
     def __init__(self):
-        URL = "mongodb://127.0.0.1:27017"
+        URL = os.getenv("URL")
         client = pymongo.MongoClient(URL)
         db_name = os.getenv("DB_NAME")
         self.db = client[db_name]
 
-    def get_all_dictionaries(self, collection_name):
+    def get_all_collections(self, collection_name):
         return self.db.collection_name.find().pretty()
 
-    def get_larger_than(self, collection_name, value):
-        return self.db.collection_name.find(value).pretty()
-
-    def insert(self, collection_name, data):
+    def insert_document(self, collection_name, data):
         self.db.collection_name.insert(data)
 
-    def delete_one(self, collection_name, value):
+    def delete_collection(self, collection_name, value):
         self.db.collection_name.delectOne(value)
 
-    def delete_many(self, collection_name, value):
+    def delete_multiple_collections(self, collection_name, value):
         self.db.collection_name.delectMany(value)
 
-    def update(self, collection_name, original, desired):
+    def update_document(self, collection_name, original, desired):
         self.db.collection_name.update(original, desired)
 
-    def find_one(self, collection_name, value):
+    def find_document(self, collection_name, value):
         return self.db.collection_name.findOne(value).pretty()
 
-    def file_folder_sharing_difference(self, collection_name, absolute_path):
-        return self.db.collection_name.find(absolute_path).pretty()
-
-    def redundant_sharing(self, collection_name, absolute_path):
-        return self.db.collection_name.find(absolute_path).pretty()
-
-    def deviant_sharing(self, collection_name, absolute_path):
-        return self.db.collection_name.find(absolute_path).pretty()
-
-    def sharing_changes(self, collection_name):
-        self.db.collection_name.find(
-            {"$where": "function(){" "return (this.file!=this.file)" "}"}
-        ).pretty()
+    def find_multiple_documents(self, collection_name, value):
+        return self.db.collection_name.find(value).pretty()

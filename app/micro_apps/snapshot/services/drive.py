f4df1a3033ea7c7d09784508b9c8c62469b4a711
YoohaBae <yooha.bae.2@gmail.com>
2022-10-11 20:37:47 -0400

    change snapshotname to body

--- app/micro_apps/snapshot/services/drive.py
+++ app/micro_apps/snapshot/services/drive.py
@@ -2,20 +2,17 @@
 Interface for Cloud Drive Authentication
 """
 import logging
-import os
 
 
 class Drive:
     def __init__(self):
-        self.SECRET = os.getenv("JWT_SECRET_KEY")
-        self.ALGORITHM = "HS256"
         self._logger = logging.getLogger(__name__)
 
     def get_files(self, creds):
         pass
 
-    def save_file_snapshot(self, creds):
+    def change_permissions(self, file, permission):
         pass
 
-    def save_group_snapshot(self, creds):
+    def change_permission(self, file, permission):
         pass

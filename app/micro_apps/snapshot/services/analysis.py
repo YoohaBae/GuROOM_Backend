6f459fde8448fe6f2adec5f26a93af2e4f1b5272
YoohaBae <yooha.bae.2@gmail.com>
2022-10-13 19:23:26 -0400

    get common and not common permissions

--- app/micro_apps/snapshot/services/analysis.py
+++ app/micro_apps/snapshot/services/analysis.py
@@ -1,4 +1,5 @@
 from .database import DataBase
+from app.utils.util import ListOfDictsComparor
 
 collection_name = "file_snapshots"
 
@@ -36,7 +37,23 @@ class Analysis:
                 self.dfs(visited, child_path, child_permissions, child_file_id)
 
     def get_sharing_differences(self, base_permissions, compare_permissions):
-        pass
+        comparor = ListOfDictsComparor()
+        base_permission_ids = [permission['id'] for permission in base_permissions]
+        compare_permission_ids = [permission['id'] for permission in compare_permissions]
+        base_permission_more_ids = comparor.difference(
+            base_permission_ids, compare_permission_ids
+        )
+        compare_permission_more_ids = comparor.difference(
+            compare_permission_ids, base_permission_ids
+        )
+        intersection_permission_ids = comparor.intersection(
+            base_permission_ids, compare_permission_ids
+        )
+        print(base_permissions)
+        print(compare_permissions)
+        print(base_permission_more_ids)
+        print(compare_permission_more_ids)
+        print(intersection_permission_ids)
 
     def compare_two_file_snapshots(self, base_snapshot_files, compare_snapshot_files):
         pass

bc3feaa48384fbd8919ad3d33a12c544a1d13bb8
YoohaBae <yooha.bae.2@gmail.com>
2022-10-14 21:33:06 -0400

    add file type

--- app/micro_apps/snapshot/endpoints/v1/google.py
+++ app/micro_apps/snapshot/endpoints/v1/google.py
@@ -5,7 +5,7 @@
 import logging
 import os
 from datetime import datetime
-from fastapi import APIRouter, status, Depends, Body, UploadFile, Form
+from fastapi import APIRouter, status, Depends, Body, UploadFile, Form, File
 from fastapi.responses import JSONResponse
 from fastapi_jwt_auth import AuthJWT
 from app.micro_apps.snapshot.services import service
@@ -29,7 +29,7 @@ logging.Formatter(
 
 @router.post("/files", tags=["file_snapshot"], status_code=status.HTTP_201_CREATED)
 def take_file_snapshot(
-    body: PostFileSnapshotBody = Body(...), authorize: AuthJWT = Depends()
+        body: PostFileSnapshotBody = Body(...), authorize: AuthJWT = Depends()
 ):
     """
     operation takes file snapshot
@@ -95,7 +95,7 @@ def take_file_snapshot(
 
 @router.delete("/files", tags=["file_snapshot"])
 def delete_file_snapshot(
-    body: DeleteFileSnapshotBody = Body(...), authorize: AuthJWT = Depends()
+        body: DeleteFileSnapshotBody = Body(...), authorize: AuthJWT = Depends()
 ):
     """
     operation: deletes file snapshot
@@ -125,7 +125,7 @@ def delete_file_snapshot(
 
 @router.put("/files", tags=["file_snapshot"])
 def edit_file_snapshot_name(
-    body: PutFileSnapshotBody = Body(...), authorize: AuthJWT = Depends()
+        body: PutFileSnapshotBody = Body(...), authorize: AuthJWT = Depends()
 ):
     """
     operation: edits file snapshots
@@ -207,14 +207,14 @@ def get_shared_drives(snapshot_name: str, authorize: AuthJWT = Depends()):
 
 @router.get("/files", tags=["file_snapshot"])
 def get_file_snapshots(
-    snapshot_name: str,
-    offset: int = None,
-    limit: int = None,
-    my_drive: bool = False,
-    shared_with_me: bool = False,
-    shared_drive: bool = True,
-    folder_id: str = None,
-    authorize: AuthJWT = Depends(),
+        snapshot_name: str,
+        offset: int = None,
+        limit: int = None,
+        my_drive: bool = False,
+        shared_with_me: bool = False,
+        shared_drive: bool = True,
+        folder_id: str = None,
+        authorize: AuthJWT = Depends(),
 ):
     """
     operation: get all files under certain folder or drive
@@ -273,9 +273,9 @@ def get_file_snapshots(
 
 @router.get("/files/search", tags=["file_snapshot"])
 def search_files(
-    snapshot_name: str,
-    query: str,
-    authorize: AuthJWT = Depends(),
+        snapshot_name: str,
+        query: str,
+        authorize: AuthJWT = Depends(),
 ):
     """
     operation: perform search on a file snapshot
@@ -323,7 +323,7 @@ def search_files(
 
 @router.get("/files/differences/sharing", tags=["file_snapshot"])
 def get_file_folder_sharing_difference(
-    snapshot_name: str, file_id: str, authorize: AuthJWT = Depends()
+        snapshot_name: str, file_id: str, authorize: AuthJWT = Depends()
 ):
     """
     operation: get the permission difference between a file and folder
@@ -364,7 +364,7 @@ def get_file_folder_sharing_difference(
 
 @router.get("/files/differences", tags=["file_snapshot"])
 def get_snapshot_difference(
-    base_snapshot_name: str, compare_snapshot_name: str, authorize: AuthJWT = Depends()
+        base_snapshot_name: str, compare_snapshot_name: str, authorize: AuthJWT = Depends()
 ):
     """
     operation: get files that are different between two file snapshots
@@ -398,11 +398,11 @@ def get_snapshot_difference(
 
 @router.post("/groups", tags=["group_snapshot"])
 async def create_group_membership_snapshot(
-    file: UploadFile,
-    group_name: str = Form(),
-    group_email: str = Form(),
-    create_time: datetime = Form(),
-    authorize: AuthJWT = Depends(),
+        file: UploadFile = File(),
+        group_name: str = Form(...),
+        group_email: str = Form(...),
+        create_time: datetime = Form(...),
+        authorize: AuthJWT = Depends(),
 ):
     authorize.jwt_required()
     access_token = authorize.get_jwt_subject()

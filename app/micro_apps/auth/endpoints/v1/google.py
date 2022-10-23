5dc4503173f2145224988a5e10e0c19f9a5aff10
YoohaBae <yooha.bae.2@gmail.com>
2022-10-23 02:58:10 -0400

    separate auth to service and api

--- app/micro_apps/auth/endpoints/v1/google.py
+++ app/micro_apps/auth/endpoints/v1/google.py
@@ -7,20 +7,14 @@ import os
 from fastapi import APIRouter, status, Depends, Body
 from fastapi.responses import JSONResponse
 from fastapi_jwt_auth import AuthJWT
-from app.micro_apps.auth.services.google_auth import GoogleAuth
+
 from app.micro_apps.auth.endpoints.models.user import User
-from app.micro_apps.auth.services.database import DataBase
+from app.micro_apps.auth.services import service
 
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
 router = APIRouter()
 router.secret_key = os.getenv("SECRET_KEY")
-
-logging.Formatter(
-    "[%(asctime)s] p%(process)s {%(pathname)s"
-    ":%(lineno)d} %(levelname)s - %(message)s",
-    "%m-%d %H:%M:%S",
-)
 
 
 @router.get(
@@ -30,39 +24,33 @@ logging.Formatter(
     status_code=status.HTTP_200_OK,
 )
 def create_google_auth():
-    google_auth = GoogleAuth()
     # get google url
-    try:
-        url = google_auth.get_authorization_url()
-        # save state to cookie
-        response = JSONResponse(status_code=status.HTTP_200_OK, content=url)
-    except Exception as error:
-        logging.error("Authorization url retrieving failed", error)
+    url = service.get_google_auth_url()
+    if url is None:
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content="unable to retrieve url",
         )
-    return response
+    return JSONResponse(status_code=status.HTTP_200_OK, content=url)
 
 
 @router.post("/login")
 def login(body=Body(...), authorize: AuthJWT = Depends()):
     code = body["code"]
-    google_auth = GoogleAuth()
-    token = google_auth.get_token(code)
-    if token:
-        access_token = authorize.create_access_token(subject=token["access_token"])
-        refresh_token = authorize.create_refresh_token(subject=token["refresh_token"])
-        response = JSONResponse(
-            status_code=status.HTTP_201_CREATED, content="token successfully created"
+    token = service.get_google_token(code)
+    if token is None:
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content="token creation failed",
         )
-        authorize.set_access_cookies(access_token, response)
-        authorize.set_refresh_cookies(refresh_token, response)
-        return response
-    return JSONResponse(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content="token creation failed",
+    access_token = authorize.create_access_token(subject=token["access_token"])
+    refresh_token = authorize.create_refresh_token(subject=token["refresh_token"])
+    response = JSONResponse(
+        status_code=status.HTTP_201_CREATED, content="token successfully created"
     )
+    authorize.set_access_cookies(access_token, response)
+    authorize.set_refresh_cookies(refresh_token, response)
+    return response
 
 
 @router.get(
@@ -88,46 +76,44 @@ def login(body=Body(...), authorize: AuthJWT = Depends()):
 def get_user(authorize: AuthJWT = Depends()):
     authorize.jwt_required()
     access_token = authorize.get_jwt_subject()
-    google_auth = GoogleAuth()
-    user = google_auth.get_user(access_token)
 
+    user = service.get_google_user(access_token)
     if user is None:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND, content="user not found"
         )
 
-    db = DataBase()
-    if db.check_user_exists(user["email"]):
-        response = JSONResponse(status_code=status.HTTP_200_OK, content=user)
+    exists = service.check_user_existence(user["email"])
+    if exists is None:
+        # unable to locate user
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content="unable to create user")
+    elif exists:
+        # user exists in our database
+        return JSONResponse(status_code=status.HTTP_200_OK, content=user)
     else:
-        db.save_user(user["email"])
-        response = JSONResponse(status_code=status.HTTP_201_CREATED, content=user)
-    return response
+        # new user
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content=user)
 
 
 @router.post("/refresh")
 def refresh_token(authorize: AuthJWT = Depends()):
     authorize.jwt_refresh_token_required()
-
     refresh_token = authorize.get_jwt_subject()
-
     authorize.unset_access_cookies()
 
-    google_auth = GoogleAuth()
-    new_token = google_auth.refresh_token(refresh_token)
-
-    if new_token:
-        new_access_token = new_token["access_token"]
-
-        new_access_token = authorize.create_access_token(new_access_token)
-        response = JSONResponse(
-            status_code=status.HTTP_200_OK, content="access token refreshed"
+    new_token = service.refresh_google_access_token(refresh_token)
+    if new_token is None:
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content="failed to refresh access token"
         )
-        authorize.set_access_cookies(new_access_token, response)
-        return response
-    return JSONResponse(
-        status_code=status.HTTP_404_NOT_FOUND, content="failed to refresh access token"
+
+    new_access_token = new_token["access_token"]
+    new_access_token = authorize.create_access_token(new_access_token)
+    response = JSONResponse(
+        status_code=status.HTTP_200_OK, content="access token refreshed"
     )
+    authorize.set_access_cookies(new_access_token, response)
+    return response
 
 
 @router.delete("/logout")
@@ -143,12 +129,12 @@ def revoke(authorize: AuthJWT = Depends()):
     authorize.jwt_required()
     access_token = authorize.get_jwt_subject()
 
-    google_auth = GoogleAuth()
-    if google_auth.revoke_token(access_token):
-        response = JSONResponse(status_code=status.HTTP_200_OK, content="token revoked")
-        authorize.unset_jwt_cookies()
-        return response
-    return JSONResponse(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        content="unable to revoke token",
-    )
+    revoked = service.revoke_google_token(access_token)
+    if not revoked:
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content="unable to revoke token",
+        )
+    response = JSONResponse(status_code=status.HTTP_200_OK, content="token revoked")
+    authorize.unset_jwt_cookies()
+    return response

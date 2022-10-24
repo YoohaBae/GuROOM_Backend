79c588ffe0add7c91062ea7240866f13f1a8d044
YoohaBae <yooha.bae.2@gmail.com>
2022-10-24 02:35:33 -0400

    implement retrieving recent queries

--- app/micro_apps/auth/services/database.py
+++ app/micro_apps/auth/services/database.py
@@ -10,7 +10,7 @@ class DataBase:
         self.collection_name = "auth"
 
     def save_user(self, email):
-        user = {"email": email}
+        user = {"email": email, "recent_queries": []}
         self._db.insert_document(self.collection_name, user)
 
     def check_user_exists(self, email):
@@ -22,5 +22,18 @@ class DataBase:
 
     def get_user(self, email):
         query = {"email": email}
-        data = self._db.find_document(self.collection_name, query)
+        filter_query = {"recent_queries": 0}
+        data = self._db.find_document(self.collection_name, query, filter_query=filter_query)
         return data
+
+    def get_recent_queries(self, email):
+        query = {"email": email}
+        filter_query = {"recent_queries": 1}
+        data = self._db.find_document(self.collection_name, query, filter_query=filter_query)
+        return data["recent_queries"]
+
+    def update_recent_queries(self, email, recent_query):
+        # TODO: if same query exist in previous array pop that element and push to most recent query
+        query = {"email": email, "recent_queries": {"$ne": recent_query}}
+        update_query = {"$push": {"recent_queries": recent_query}}
+        self._db.update_document(self.collection_name, update_query, query)

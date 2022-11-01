cd022af57826b6bf06f16b4a4ccaf899f718446e
YoohaBae <yooha.bae.2@gmail.com>
2022-11-01 18:22:06 -0400

    refactor auth

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -2,25 +2,60 @@ import logging
 
 
 class AuthService:
-    def __init__(self):
+    def __init__(self, auth, db):
         format = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
         logging.basicConfig(format=format)
         self.logger = logging.getLogger()
+        self.auth = auth
+        self._db = db
 
     def get_auth_url(self):
-        pass
+        try:
+            url = self.auth.get_authorization_url()
+            return url
+        except Exception as error:
+            self.logger.error(error)
+            return None
 
     def get_token(self, code):
-        pass
+        try:
+            token = self.auth.get_token(code)
+            return token
+        except Exception as error:
+            self.logger.error(error)
+            return None
 
     def get_user(self, access_token):
-        pass
+        try:
+            user = self.auth.get_user(access_token)
+            return user
+        except Exception as error:
+            self.logger.error(error)
+            return None
 
     def check_user_existence(self, email):
-        pass
+        try:
+            if self._db.check_user_exists(email):
+                return True
+            else:
+                self._db.save_user(email)
+                return False
+        except Exception as error:
+            self.logger.error(error)
+            return None
 
     def refresh_access_token(self, refresh_token):
-        pass
+        try:
+            new_token = self.auth.refresh_token(refresh_token)
+            return new_token
+        except Exception as error:
+            self.logger.error(error)
+            return None
 
     def revoke_token(self, access_token):
-        pass
+        try:
+            revoked = self.auth.revoke_token(access_token)
+            return revoked
+        except Exception as error:
+            self.logger.error(error)
+            return False
